7370ed4b738ec54bf1ca819ca2faa75d1e632126
Gleb <skryabin.gleb.d@gmail.com>
2023-02-05 19:17:08 +0300

    trainers-button

--- mobile/play_tennis/README.md
+++ mobile/play_tennis/README.md
@@ -1,22 +1,17 @@
-# play_tennis
+# Play tennis
 
-A new Flutter project.
+## Для андроида
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-
-# Для андроида
+```bash
 flutter build apk --split-per-abi --release --no-sound-null-safety
+```
 
-# Для айфона
+## Для айфона
+
+```bash
 flutter run --release --no-sound-null-safety
+```
+
+## Swagger
+
+<https://api.play-tennis.online/swagger/index.html>
---
+++ mobile/play_tennis/lib/app/ptc/screens/TrainersPage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:play_tennis/app/main/widgets/side_drawer.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class TrainersPage extends StatelessWidget {
+  const TrainersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Тренеры"),
+      ),
+      drawer: const SideDrawer(),
+      body: Center(
+        child: RichText(
+          text: TextSpan(
+            text: 'Перейти на сайт',
+            style: const TextStyle(
+              color: Colors.blue,
+              fontSize: 18,
+              decoration: TextDecoration.underline,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () => _launchUrl(
+                    Uri.parse("https://play-tennis.online/ptc/trainers"),
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw 'Could not launch $url';
+    }
+  }
+}
