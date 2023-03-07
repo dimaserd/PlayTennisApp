d23cad0e83ad61e53ef34927a828f1a0308f8eb4
dimaserd84@gmail.com <dimaserd84@gmail.com>
2023-03-07 21:55:34 +0300

    Fixes

--- mobile/play_tennis/lib/main-settings.dart
+++ mobile/play_tennis/lib/main-settings.dart
@@ -3,7 +3,7 @@ class MainSettings {
 
   static const String appName = "Play Tennis";
 
-  static const String imageLogoPath = "assets/images/logo.png";
+  static const String imageLogoPath = "assets/icons/icon.png";
 
   static const String loadingPath = "assets/icons/icon.png";
 
