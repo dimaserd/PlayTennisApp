4844970108160f26ac7ed509212d7517be0fb172
dimaserd84@gmail.com <dimaserd84@gmail.com>
2023-04-02 23:26:22 +0300

    Fixes

---
+++ mobile/play_tennis/lib/app/ptc/widgets/cities/SelectCityDistrictsWidget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:play_tennis/logic/ptc/services/CityService.dart';
+import 'package:play_tennis/main-services.dart';
+import 'package:play_tennis/main.dart';
+
+class SelectCityDistrictsWidget extends StatefulWidget {
+  final String cityId;
+  const SelectCityDistrictsWidget({required this.cityId, super.key});
+
+  @override
+  State<SelectCityDistrictsWidget> createState() =>
+      _SelectCityDistrictsWidgetState();
+}
+
+class _SelectCityDistrictsWidgetState extends State<SelectCityDistrictsWidget> {
+  List<CityDistrictModel> districts = [];
+
+  @override
+  void initState() {
+    getDistricts();
+    super.initState();
+  }
+
+  getDistricts() async {
+    var value = await AppServices.cityService
+        .getCityDistricts(widget.cityId, (p0) => {});
+    setState(() {
+      districts = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- mobile/play_tennis/lib/logic/ptc/services/CityService.dart
+++ mobile/play_tennis/lib/logic/ptc/services/CityService.dart
@@ -1,6 +1,27 @@
 import 'dart:convert';
-import '../../core/NetworkService.dart';
-import '../models/cities/PublicTelegramChatForCityModel.dart';
+import 'package:play_tennis/logic/core/NetworkService.dart';
+import 'package:play_tennis/logic/ptc/models/cities/PublicTelegramChatForCityModel.dart';
+
+class CityDistrictModel {
+  late String? id;
+  late String? name;
+
+  CityDistrictModel({
+    required this.id,
+    required this.name,
+  });
+
+  factory CityDistrictModel.fromJson(Map<String, dynamic> json) =>
+      CityDistrictModel(
+        id: json["id"],
+        name: json["name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+      };
+}
 
 class CityService {
   final NetworkService networkService;
@@ -22,5 +43,23 @@ class CityService {
     var json = jsonDecode(response!);
 
     return PublicTelegramChatForCityModel.fromJson(json);
+  }
+
+  Future<List<CityDistrictModel>> getCityDistricts(
+    String cityId,
+    Function(String) onError,
+  ) async {
+    var response = await networkService.getDataInner(
+      '/api/ptc/dlv/city-district/query/by-city/{cityId}/cached',
+      onError,
+    );
+
+    if (response == "null") {
+      return [];
+    }
+
+    var json = jsonDecode(response!);
+
+    return json.map((x) => CityDistrictModel.fromJson(x));
   }
 }
