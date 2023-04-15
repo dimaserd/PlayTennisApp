10d6142c52ba14ac5479cfbaf33bea024b4fe1b5
dimaserd84@gmail.com <dimaserd84@gmail.com>
2023-04-15 15:38:16 +0300

    Fixes

--- mobile/play_tennis/lib/app/ptc/screens/GamesRequestsScreen.dart
+++ mobile/play_tennis/lib/app/ptc/screens/GamesRequestsScreen.dart
@@ -28,7 +28,7 @@ class _GamesRequestsScreenState extends State<GamesRequestsScreen> {
   }
 
   getData() {
-    AppServices.playerService.getLocationData().then((value) {
+    AppServices.playerService.getLocationData((e) => {}).then((value) {
       if (value == null) {
         BaseApiResponseUtils.showError(context, "Кажется вы были разлогинены");
         Navigator.of(context)
