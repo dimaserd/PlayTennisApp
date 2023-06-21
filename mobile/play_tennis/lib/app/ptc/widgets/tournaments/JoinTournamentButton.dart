488020a4609339e079153cbe75e803addd6e13ed
Nikita <35070542+tommy-tom@users.noreply.github.com>
2023-06-21 22:51:24 +0300

    add new feature

--- mobile/play_tennis/lib/app/ptc/widgets/tournaments/JoinTournamentButton.dart
+++ mobile/play_tennis/lib/app/ptc/widgets/tournaments/JoinTournamentButton.dart
@@ -2,44 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:play_tennis/baseApiResponseUtils.dart';
 import 'package:play_tennis/logic/ptc/services/TournamentPlayerService.dart';
 import 'package:play_tennis/main-services.dart';
+import 'package:play_tennis/main-settings.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class JoinTournamentButton extends StatelessWidget {
+class JoinTournamentButton extends StatefulWidget {
   final int participationCostRub;
   final String tournamentId;
+
   const JoinTournamentButton({
     super.key,
     required this.participationCostRub,
     required this.tournamentId,
   });
+  @override
+  State<JoinTournamentButton> createState() => _JoinTournamentButton();
+}
+
+class _JoinTournamentButton extends State<JoinTournamentButton> {
+  bool isHidePaymentButton = true;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.blue,
-        minimumSize: const Size.fromHeight(36),
-      ),
-      onPressed: () {
-        clickHandler(context);
-      },
-      child: Text(
-        "Записаться $participationCostRub₽",
+    return Visibility(
+      visible: isHidePaymentButton,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          minimumSize: const Size.fromHeight(36),
+        ),
+        onPressed: () {
+          clickHandler(context);
+        },
+        child: Text(
+          "Записаться ${widget.participationCostRub}",
+        ),
       ),
     );
   }
 
   clickHandler(BuildContext context) {
-    AppServices.tournamentPlayerService.join(tournamentId).then((value) {
-      if (!value.isSucceeded) {
-        BaseApiResponseUtils.showError(context, value.message);
-      } else {
+    AppServices.tournamentPlayerService.join(widget.tournamentId).then((value) {
+      if (value.isSucceeded) {
         BaseApiResponseUtils.showSuccess(context, value.message);
-
+      } else {
         if (value.responseObject!.errorType ==
             TournamentJoiningErrorType.NotEnoughMoney) {
-          //TODO Показать голубую кнопку (Пополнить баланс) эта кнопка откроет браузер
+          BaseApiResponseUtils.showError(context, value.message);
+          var uri = Uri.parse(MainSettings.domain);
+          launchUrl(uri);
         } else {
-          //TODO Скрыть кнопку
+          setState(() {
+            isHidePaymentButton = false;
+          });
         }
       }
     });
