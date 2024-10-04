08b5bdea9ba8f7c46b1019ff936b8bd2fbfa628e
alaamashkouk <alaamashkouk2@gmail.com>
2024-10-05 00:11:48 +0300

    FIX: rotating images

--- mobile/play_tennis/lib/app/ptc/screens/takePhotoScreen.dart
+++ mobile/play_tennis/lib/app/ptc/screens/takePhotoScreen.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
+import 'package:image/image.dart' as img;
+import 'package:sensors_plus/sensors_plus.dart';
 
 class TakePhotoScreen extends StatefulWidget {
   const TakePhotoScreen({super.key});
@@ -18,11 +21,34 @@ class _TakePhotoScreenState extends State<TakePhotoScreen> {
 
   late CameraDescription selectedCamera;
   late List<CameraDescription> cameras;
+  bool isVertical = true;
+  StreamSubscription<AccelerometerEvent>? _accelerometerSubscription;
 
   @override
   void initState() {
     super.initState();
     _initializeCamera();
+    _listenToOrientation();
+  }
+
+  void _listenToOrientation() {
+    _accelerometerSubscription =
+        // accelerometerEvents.listen((AccelerometerEvent event) {
+        accelerometerEventStream().listen((AccelerometerEvent event) {
+      if (event.x.abs() > event.y.abs()) {
+        if (mounted) {
+          setState(() {
+            isVertical = false;
+          });
+        }
+      } else {
+        if (mounted) {
+          setState(() {
+            isVertical = true;
+          });
+        }
+      }
+    });
   }
 
   Future<void> _initializeCamera({CameraDescription? theSelectedCamera}) async {
@@ -43,6 +69,44 @@ class _TakePhotoScreenState extends State<TakePhotoScreen> {
     }
   }
 
+  Future<XFile> rotate(image, imagePath) async {
+    final imageBytes = await File(image.path).readAsBytes();
+    img.Image capturedImage = img.decodeImage(imageBytes)!;
+
+    if (!isVertical) {
+      capturedImage = img.copyRotate(capturedImage, angle: 90);
+    }
+
+    double targetAspectRatio = 3 / 4;
+
+    double currentAspectRatio = capturedImage.width / capturedImage.height;
+
+    if (currentAspectRatio > targetAspectRatio) {
+      int newWidth = (capturedImage.height * targetAspectRatio).toInt();
+      int offsetX = (capturedImage.width - newWidth) ~/ 2;
+      capturedImage = img.copyCrop(capturedImage,
+          x: offsetX, y: 0, width: newWidth, height: capturedImage.height);
+    } else if (currentAspectRatio < targetAspectRatio) {
+      int newHeight = (capturedImage.width / targetAspectRatio).toInt();
+      int offsetY = (capturedImage.height - newHeight) ~/ 2;
+      capturedImage = img.copyCrop(capturedImage,
+          x: 0, y: offsetY, width: capturedImage.width, height: newHeight);
+    }
+    int targetWidth = 800;
+    int targetHeight = (targetWidth / targetAspectRatio).toInt();
+
+    img.Image resizedImage = img.copyResize(
+      capturedImage,
+      width: targetWidth,
+      height: targetHeight,
+      interpolation: img.Interpolation.linear,
+    );
+
+    final jpgBytes = img.encodeJpg(resizedImage);
+    File imageFile = await File(imagePath).writeAsBytes(jpgBytes);
+    return XFile(imageFile.path);
+  }
+
   Future<void> _takePicture() async {
     if (_controller == null || !_controller!.value.isInitialized) {
       print('Controller is not initialized');
@@ -53,16 +117,13 @@ class _TakePhotoScreenState extends State<TakePhotoScreen> {
       await _initializeControllerFuture;
 
       final directory = await getApplicationDocumentsDirectory();
-      final imagePath = path.join(directory.path, '${DateTime.now()}.png');
+      final imagePath = path.join(directory.path, '${DateTime.now()}.jpg');
 
       XFile image = await _controller!.takePicture();
+      XFile imageFile = await rotate(image, imagePath);
       setState(() {
-        _imageFile = image;
+        _imageFile = XFile(imageFile.path);
       });
-
-      await image.saveTo(imagePath);
-
-      /// Close the camera after taking the picture
       _controller!.dispose();
     } catch (e) {
       print(e);
@@ -72,6 +133,7 @@ class _TakePhotoScreenState extends State<TakePhotoScreen> {
   @override
   void dispose() {
     _controller?.dispose();
+    _accelerometerSubscription?.cancel();
     super.dispose();
   }
 
