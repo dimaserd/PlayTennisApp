170cb9ec81da629f66bfa2a08f3c435a494b318f
alaamashkouk <alaamashkouk2@gmail.com>
2024-11-02 12:03:04 +0300

    FIX :improve performance and fix glitch

--- mobile/play_tennis/lib/app/main/widgets/images/ImageViewWidget.dart
+++ mobile/play_tennis/lib/app/main/widgets/images/ImageViewWidget.dart
@@ -33,6 +33,10 @@ class ImageViewWidgetExtensions {
     return _buildUrl(fileId, "Medium");
   }
 
+  static String buildThumbnailUrl(int fileId) {
+    return _buildUrl(fileId, "Small");
+  }
+
   static Scaffold getScaffold(String originalUrl) {
     return Scaffold(
       key: GlobalKey<ScaffoldState>(),
@@ -40,9 +44,7 @@ class ImageViewWidgetExtensions {
         title: const Text("Просмотр изображения"),
       ),
       body: ImageViewRouteWrapper(
-        imageProvider: NetworkImage(
-          originalUrl,
-        ),
+        imageProvider: NetworkImage(originalUrl),
         loadingBuilder: (context, event) {
           if (event == null) {
             return const Center(
@@ -83,7 +85,7 @@ class _ImageViewWidgetState extends State<ImageViewWidget> {
 
     if (_cachedImage == null) {
       _imageFuture = _loadAndCorrectImage(
-              ImageViewWidgetExtensions.buildMediumUrl(widget.fileId))
+              ImageViewWidgetExtensions.buildThumbnailUrl(widget.fileId))
           .then((image) {
         /// Cache the image
         _cachedImage = image;
@@ -99,50 +101,44 @@ class _ImageViewWidgetState extends State<ImageViewWidget> {
     /// Fetch the image from the server
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      /// Save the image temporarily to a file
-      final tempDir = Directory.systemTemp;
-      final tempFile = File('${tempDir.path}/temp_image_${widget.fileId}.png');
-      await tempFile.writeAsBytes(response.bodyBytes);
-
-      /// Read EXIF data from the image
       try {
+        /// Save the image temporarily to a file
+        final tempDir = Directory.systemTemp;
+        final tempFile =
+            File('${tempDir.path}/temp_image_${widget.fileId}.png');
+        await tempFile.writeAsBytes(response.bodyBytes);
+
+        /// Read EXIF data from the image
         final tags =
             await exif_reader.readExifFromBytes(await tempFile.readAsBytes());
 
         /// Load the image
         img.Image? image = img.decodeImage(response.bodyBytes);
 
-        if (image == null) {
-          throw Exception("Error decoding the image");
-        }
+        if (image == null) throw Exception("Error decoding the image");
 
-        for (final entry in tags.entries) {
-          if (entry.key == 'Image Orientation' && tags.isNotEmpty) {
-            String orientation = entry.value.toString();
+        if (tags.isNotEmpty) {
+          String? orientation = tags['Image Orientation']?.toString();
 
-            /// Rotate the image based on EXIF orientation
+          /// Rotate the image based on EXIF orientation
+          if (orientation != null) {
             switch (orientation) {
               case 'Mirrored vertical':
-                image = img.copyRotate(image!, angle: 180);
+                image = img.copyRotate(image, angle: 180);
                 break;
               case 'Rotated 90 CW':
-                image = img.copyRotate(image!, angle: 90);
-                break;
-              default:
+                image = img.copyRotate(image, angle: 90);
                 break;
             }
 
             /// Convert the rotated image back to bytes
-            Uint8List correctedBytes =
-                Uint8List.fromList(img.encodePng(image!));
-
-            /// Return the corrected image
+            Uint8List correctedBytes = Uint8List.fromList(img.encodePng(image));
             return Image.memory(correctedBytes);
-          } else {
-            /// If no orientation tag, return the original image
-            return Image.memory(response.bodyBytes);
           }
         }
+
+        /// If no orientation tag, return the original image
+        return Image.memory(response.bodyBytes);
       } catch (e) {
         print('Error reading EXIF or rotating image: $e');
         return Image.memory(response.bodyBytes);
@@ -161,9 +157,9 @@ class _ImageViewWidgetState extends State<ImageViewWidget> {
         future: _imageFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Container();
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            print('snapshot${snapshot.error}');
+            print('Error loading image: ${snapshot.error}');
             return const Text('Error loading image');
           } else {
             return snapshot.data!;
@@ -184,7 +180,7 @@ class ImageViewRouteWrapper extends StatelessWidget {
     this.maxScale,
     this.initialScale,
     this.basePosition = Alignment.center,
-    this.filterQuality = FilterQuality.none,
+    this.filterQuality = FilterQuality.low,
     this.disableGestures,
     this.errorBuilder,
   });
